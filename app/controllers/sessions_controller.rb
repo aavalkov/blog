3607f3c58de79620c5e95c2cf9af639445306383
Anna Valkov <anna.a.valkov@gmail.com>
2014-09-02 11:58:19 -0700

    display which user is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url
-      flash[:notice] = "Logged in."
+      redirect_to root_url, notice: "Logged in."
     else
       flash.now.alert = "Email or password invalid"
       render 'new'
