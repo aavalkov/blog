97aafe33c295314888ed4961e6a6136b742f51d2
Anna Valkov <anna.a.valkov@gmail.com>
2014-09-02 20:44:44 -0700

    add button for new post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to users_path
+      redirect_to root_url
       flash[:notice] = "login successful"
     else
       render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
-  root :to => 'users#index'
+  root :to => 'posts#index'
   resources :users
   resources :sessions
   resources :posts
