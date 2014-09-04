59fce4e791d8780336789ba9350d3b323c316d5a
Anna Valkov <anna.a.valkov@gmail.com>
2014-09-04 14:41:17 -0700

    check the route for invalid sign in

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -9,5 +9,15 @@ describe "the signin process" do
     click_button 'Log In'
     page.should have_content 'Code Tips'
   end
+
+  it "doesn't sign in a user with the wrong password" do
+      visit '/login'
+      user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => ''
+      click_button 'Log In'
+      page.should have_content 'invalid'
+  end
+
 end
 
