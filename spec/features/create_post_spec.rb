2419972e20ebda3f90cc5f0d3e2cd30bfb0e34de
Anna Valkov <anna.a.valkov@gmail.com>
2014-09-04 15:45:28 -0700

    ensure path for unsuccessful posts works

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -14,4 +14,19 @@ describe 'the add a new post process' do
     click_button 'Create Post'
     page.should have_content 'Saved'
   end
+
+   it "doesn't create a new post if all of the information isn't added" do
+    visit '/login'
+    user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log In'
+    page.should have_content 'Code Tips'
+    visit 'posts/new'
+    fill_in 'post_name', :with => ''
+    fill_in 'post_article', :with => 'This is some example text'
+    click_button 'Create Post'
+    page.should have_content 'error'
+  end
+
 end
